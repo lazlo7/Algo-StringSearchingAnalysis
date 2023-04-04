a61db33ec52e3803d8310cd99b968ede96c3beb0
Max Smagin <lazlogogalac777@gmail.com>
2023-04-04 17:56:53 +0300

    Made util::getRandomNumber a constexpr

--- src/stringer/util.hpp
+++ src/stringer/util.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <random>
+#include <type_traits>
 
 namespace util {
 static std::mt19937 _random_generator { std::random_device()() };
@@ -8,7 +9,7 @@ static std::uniform_real_distribution<double> _random_real_distribution { 0.0, 1
 
 template <typename T>
 requires std::is_integral_v<T>
-T getRandomNumber(T min, T max)
+constexpr T getRandomNumber(T min, T max)
 {
     return std::round(_random_real_distribution(_random_generator) * (max - min)) + min;
 }
