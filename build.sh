3728ffa07084272880a58f0b7e320e133489f97f
Max Smagin <lazlogogalac777@gmail.com>
2023-04-06 22:18:36 +0300

    Added seacrher.cpp to build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,12 @@ set -xeu
 CC="g++"
 CFLAGS="-Wall -Wextra -pedantic -std=c++20 -O3"
 SRC_DIR="src/stringer"
-SOURCES="${SRC_DIR}/main.cpp ${SRC_DIR}/tester.cpp ${SRC_DIR}/searcher/naive_searcher.cpp ${SRC_DIR}/text_generator/uniform_text_generator.cpp"
+SOURCES="${SRC_DIR}/main.cpp \
+         ${SRC_DIR}/tester.cpp \
+         ${SRC_DIR}/searcher/searcher.cpp \
+         ${SRC_DIR}/searcher/naive_searcher.cpp \
+         ${SRC_DIR}/searcher/kmp_searcher.cpp \
+         ${SRC_DIR}/text_generator/uniform_text_generator.cpp"
 OUTPUT="stringer"
 OUTPUT_DIR="./build"
 
