c1e402d1fe514d1102effe43612451445fca5400
Max Smagin <lazlogogalac777@gmail.com>
2023-04-06 03:07:33 +0300

    Added size_t parsing for cmd-line arguments

--- src/stringer/main.cpp
+++ src/stringer/main.cpp
@@ -1,29 +1,46 @@
 #include "tester.hpp"
 
 #include <iostream>
-#include <stdexcept>
+#include <sstream>
+#include <string>
+
+static size_t parseSizeT(int argc, char** args, const std::string& arg_prefix, size_t default_value)
+{
+    for (int i = 0; i < argc; ++i) {
+        std::string arg { args[i] };
+        if (arg.starts_with(arg_prefix)) {
+            const std::string to_parse = arg.substr(arg_prefix.size());
+            if (to_parse.empty()) {
+                return default_value;
+            }
+
+            std::stringstream stream_to_parse { to_parse };
+            size_t value;
+            stream_to_parse >> value;
+
+            if (stream_to_parse.fail()) {
+                return default_value;
+            }
+
+            return value;
+        }
+    }
+
+    return default_value;
+}
 
 int main(int argc, char** argv)
 {
     if (argc < 2) {
-        std::cout << "Usage: ./stringer <output_filename> [<test_repeat_count>]" << '\n';
+        std::cout << "Usage: ./stringer <output_filename> [-tr=<test_repeat_count>]" << '\n'
+                  << "<output_filename> - filename to write results to,\n"
+                  << "<test_repeat_count> - how many times each test should be repeated [default = 100]\n";
+
         return 1;
     }
 
     const char* output_filename = argv[1];
-
-    size_t test_repeat_count = 100;
-    if (argc >= 3) {
-        try {
-            test_repeat_count = std::stoull(argv[2]);
-        } catch (const std::invalid_argument& e) {
-            std::cout << "[Error] Invalid test_repeat_count: must be a positive integer!\n";
-            return 1;
-        } catch (const std::out_of_range& e) {
-            std::cout << "[Error] Invalid test_repeat_count: provided value is too large!\n";
-            return 1;
-        }
-    }
+    const auto test_repeat_count = parseSizeT(argc, argv, "-tr=", 100);
 
     Tester tester { test_repeat_count };
     tester.runTests(output_filename);
