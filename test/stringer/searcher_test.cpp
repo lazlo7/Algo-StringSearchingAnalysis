64f4be4e626e53cf3308a7196f2af827ba2409a4
Max Smagin <lazlogogalac777@gmail.com>
2023-04-07 18:38:34 +0300

    Added proper tester for searchers

---
+++ test/stringer/searcher_test.cpp
@@ -0,0 +1,35 @@
+#include "../../src/stringer/searcher/kmp_searcher.hpp"
+#include "../../src/stringer/searcher/naive_searcher.hpp"
+#include "../../src/stringer/searcher/searcher.hpp"
+
+#include <iostream>
+#include <memory>
+
+void test(const std::shared_ptr<Searcher>& searcher, const std::string& text, const std::string& pattern)
+{
+    std::cout << "Searcher: " << searcher->name() << '\n';
+
+    size_t char_comparisons {};
+    const auto result = searcher->search(text, pattern, char_comparisons);
+    
+    std::cout << "Char comparisons: " << char_comparisons << '\n';
+    for (const auto index : result) {
+        std::cout << index << ' ';
+    }
+    std::cout << '\n';
+}
+
+int main()
+{
+    std::string text;
+    std::cin >> text;
+
+    std::string pattern;
+    std::cin >> pattern;
+
+    test(std::make_shared<NaiveSearcher>(), text, pattern);
+    test(std::make_shared<KMPSearcher>(), text, pattern);
+    test(std::make_shared<KMPPreciseBordersSearcher>(), text, pattern);
+
+    return 0;
+}
