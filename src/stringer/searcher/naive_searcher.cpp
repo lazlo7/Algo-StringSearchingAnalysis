6f67035421639296113b2def2cc34999ba62d201
Max Smagin <lazlogogalac777@gmail.com>
2023-04-07 14:43:08 +0300

    Fixed naive searcher algorithm

--- src/stringer/searcher/naive_searcher.cpp
+++ src/stringer/searcher/naive_searcher.cpp
@@ -7,24 +7,19 @@ Searcher::Indices NaiveSearcher::search(
 {
     Searcher::Indices indices;
 
-    for (size_t i = 0; i < text.size(); ++i) {
+    for (size_t text_index = 0; text_index < text.size(); ++text_index) {
         bool found = true;
-        for (size_t j = 0; j < pattern.size(); ++j) {
-            if (i + j + 1 > text.size()) {
-                found = false;
-                break;
-            }
 
-            ++char_comparisons;
-            const auto ch = text[i + j];
-            if (ch != pattern[j] || ch != '?') {
+        for (size_t pattern_index = 0; pattern_index < pattern.size(); ++pattern_index) {
+            if (text_index + pattern_index >= text.size()
+                || !compare(text[text_index + pattern_index], pattern[pattern_index], char_comparisons)) {
                 found = false;
                 break;
             }
         }
 
         if (found) {
-            indices.push_back(i);
+            indices.push_back(text_index);
         }
     }
 
